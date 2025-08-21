2e37426b7678398c070444534653decab164ac78
Ivan Roman <DigitalHerencia@Outlook.com>
2025-08-21 08:27:17 -0700

    chore: configure tailwindcss with animation plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+import animate from 'tailwindcss-animate'
+
+export default {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './features/**/*.{ts,tsx}',
+    './hooks/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+} satisfies Config
